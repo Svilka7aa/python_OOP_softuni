eebc656be285340860b3b32fe4b24d3b940adfe6
Svilen Petrov <102098862+Svilka7aa@users.noreply.github.com>
2022-07-28 17:45:34 +0200

    Delete inheritance_exercise/need_for_speed directory

--- inheritance_exercise/need_for_speed/project/family_car.py
+++
@@ -1,6 +0,0 @@
-from project.car import Car
-
-
-class FamilyCar(Car):
-    def __init__(self, fuel, hors_power):
-        super().__init__(fuel, hors_power)
--- inheritance_exercise/need_for_speed/project/sport_car.py
+++
@@ -1,8 +0,0 @@
-from project.car import Car
-
-
-class SportCar(Car):
-    DEFAULT_FUEL_CONSUMPTION = 10
-
-    def __init__(self, fuel, hors_power):
-        super().__init__(fuel, hors_power)
